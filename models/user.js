0c3a048941db247ac1b341f02fa220db945283e4
Lukas <lukasdrinkwater97@gmail.com>
2023-12-02 15:29:39 +0000

    most recent changes for debug

--- models/user.js
+++ models/user.js
@@ -13,17 +13,17 @@ const UserSchema = new Schema({
 
 // middleware
 
-UserSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+// UserSchema.pre("save", async function (next) {
+//   if (!this.isModified("password")) return next();
 
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    return next();
-  } catch (error) {
-    return next(error);
-  }
-});
+//   try {
+//     const salt = await bcrypt.genSalt(10);
+//     this.password = await bcrypt.hash(this.password, salt);
+//     return next();
+//   } catch (error) {
+//     return next(error);
+//   }
+// });
 
 // virtual for url etc
 
