d23dced5387ee9df2009207bf365a59cf7ba20c1
Lukas <lukasdrinkwater97@gmail.com>
2023-12-08 12:12:31 +0000

    added favicon require in app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const favicon = require("serve-favicon");
 
 // PASSPORT requirements
 const passport = require("passport");
